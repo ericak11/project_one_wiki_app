b9d76b5b2faabeb54d3f92f19de3dc970627c325
ericak11 <kantor.erica@gmail.com>
2014-09-10 12:07:39 -0400

    updated no checked boex problem

--- spec/main_page_spec.rb
+++ spec/main_page_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 require 'rack_session_access/capybara'
-require 'rack_session_access'
 
 feature "My feature" do
   background do
-    # binding.pry
-    # @user = Factory(:current_user )
+    current_user = {
+      user_id: 12345,
+      name: "Sally"
+    }
+    page.set_rack_session(:current_user => current_user)
   end
 
 
 
   scenario "logged in user access profile page" do
-    binding.pry
-    page.set_rack_session(:user_id => "12345")
     page.visit "/users/12345"
     page.should have_content("Sally's Posts")
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,14 +6,14 @@ require 'rack_session_access'
 require './app'
 require 'pry'
 
+App.configure do |app|
+  app.use RackSessionAccess::Middleware
+end
+
 Capybara.app = App
 
 RSpec.configure do |config|
   config.include Capybara::DSL
 end
 
-binding.pry
 
-App.configure do |app|
-  app.use RackSessionAccess::Middleware
-end
